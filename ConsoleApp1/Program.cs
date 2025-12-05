8b83838ad67f31dea935425de40ded4d70200ffb
TheGenius <TheGenius@DESKTOP-Q6JVHKA>
2025-12-05 19:10:50 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 10;
+            int Y = 20;
+
+           
+            Console.WriteLine($"Equation: {X} + {Y} = {(X + Y):C}");
+
+          
+            double num = 1234.567;
+            Console.WriteLine($"Number format (N): {num:N}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
